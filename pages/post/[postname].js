9f8c249a167571bd5bf937ce9ca3ef3913ed0ef5
Anden Acitelli <a.acitelli@gmail.com>
2021-05-20 15:49:45 -0400

    Update tips message

--- pages/post/[postname].js
+++ pages/post/[postname].js
@@ -30,13 +30,13 @@ export default function BlogPost({ siteTitle, frontmatter, markdownBody }) {
           <div>
             <ReactMarkdown source={markdownBody} />
           </div>
-          <h3>Check Me Out</h3>
+          <h3>Information</h3>
           <ul>
             <li><Link href="/blog"><a>List of Articles I've Written</a></Link></li>
             <li>Get in touch at <Link href="mailto:andenacitelli@gmail.com"><a>andenacitelli@gmail.com</a></Link></li>
             <li>GitHub <Link href="https://github.com/aacitelli"><a>@aacitelli</a></Link>, LinkedIn <Link href="https://linkedin.com/in/anden-acitelli"><a>/in/anden-acitelli</a></Link></li>
-            <li>If my content was helpful to you, please consider <Link href="https://paypal.me/andenacitelli"><a>tipping me via Paypal</a></Link>.</li>
           </ul>
+          <p><li>I try to write concise articles that aren't a waste of time - please consider <Link href="https://paypal.me/andenacitelli"><a>tipping me via Paypal</a></Link> or turning off your adblocker if you got a lot of value from the article. Regardless, thanks for reading!</li></p>
         </article>
       </Layout>
       <style jsx>{`
